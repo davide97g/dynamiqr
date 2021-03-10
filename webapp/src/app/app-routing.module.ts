dcc49ecf5e58274c1894ac9ef70ad201dc355569
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-03-11 00:32:17 +0100

    routes + pages

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { OrderComponent } from './pages/order/order.component'
+import { PersonalAreaComponent } from './pages/personal-area/personal-area.component'
+import { RedirectComponent } from './pages/redirect/redirect.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: 'redirect',
+        component: RedirectComponent,
+    },
+    {
+        path: 'personal-area',
+        component: PersonalAreaComponent,
+    },
+    {
+        path: 'order',
+        component: OrderComponent,
+    },
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', component: HomeComponent },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { RedirectComponent } from './pages/redirect/redirect.component';
+import { PersonalAreaComponent } from './pages/personal-area/personal-area.component';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RedirectComponent,
+    PersonalAreaComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
