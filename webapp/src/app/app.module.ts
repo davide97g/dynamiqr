5a392799ef019a5a7406e5842c22294b2773a324
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-03-12 00:37:39 +0100

    created structure + pages

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -13,6 +13,11 @@ import { ProgressBarComponent } from './components/progress-bar/progress-bar.com
 import { PopupComponent } from './components/popup/popup.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { TermsOfServiceComponent } from './pages/terms-of-service/terms-of-service.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { ShirtsComponent } from './pages/shirts/shirts.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +30,12 @@ import { FooterComponent } from './components/footer/footer.component';
     ProgressBarComponent,
     PopupComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdminComponent,
+    NotFoundComponent,
+    TermsOfServiceComponent,
+    PrivacyPolicyComponent,
+    ShirtsComponent
   ],
   imports: [
     BrowserModule,
