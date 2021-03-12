996019470b0b424774cc76fb875996edd6da1750
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-03-12 18:02:39 +0100

    auth + login/logout

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,48 +1,68 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-import { RedirectComponent } from './pages/redirect/redirect.component';
-import { PersonalAreaComponent } from './pages/personal-area/personal-area.component';
-import { OrderComponent } from './pages/order/order.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
-import { PopupComponent } from './components/popup/popup.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AdminComponent } from './pages/admin/admin.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { TermsOfServiceComponent } from './pages/terms-of-service/terms-of-service.component';
-import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
-import { ShirtsComponent } from './pages/shirts/shirts.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HomeComponent } from './pages/home/home.component'
+import { RedirectComponent } from './pages/redirect/redirect.component'
+import { PersonalAreaComponent } from './pages/personal-area/personal-area.component'
+import { OrderComponent } from './pages/order/order.component'
+import { MenuComponent } from './components/menu/menu.component'
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component'
+import { PopupComponent } from './components/popup/popup.component'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { AdminComponent } from './pages/admin/admin.component'
+import { NotFoundComponent } from './pages/not-found/not-found.component'
+import { TermsOfServiceComponent } from './pages/terms-of-service/terms-of-service.component'
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component'
+import { ShirtsComponent } from './pages/shirts/shirts.component'
+
+/** @angular/fire */
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+import { AngularFireAuthModule } from '@angular/fire/auth'
+
+const config = {
+    apiKey: 'AIzaSyDL7OR5zrqJXXhZV8tRP80_YN5SRxERr1k',
+    authDomain: 'dynamiqr.firebaseapp.com',
+    databaseURL:
+        'https://dynamiqr-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'dynamiqr',
+    storageBucket: 'dynamiqr.appspot.com',
+    messagingSenderId: '123071531989',
+    appId: '1:123071531989:web:91f7d42983387b970c0b5c',
+    measurementId: 'G-P8K36P1EP7',
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    RedirectComponent,
-    PersonalAreaComponent,
-    OrderComponent,
-    MenuComponent,
-    ProgressBarComponent,
-    PopupComponent,
-    HeaderComponent,
-    FooterComponent,
-    AdminComponent,
-    NotFoundComponent,
-    TermsOfServiceComponent,
-    PrivacyPolicyComponent,
-    ShirtsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        RedirectComponent,
+        PersonalAreaComponent,
+        OrderComponent,
+        MenuComponent,
+        ProgressBarComponent,
+        PopupComponent,
+        HeaderComponent,
+        FooterComponent,
+        AdminComponent,
+        NotFoundComponent,
+        TermsOfServiceComponent,
+        PrivacyPolicyComponent,
+        ShirtsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        AngularFireModule.initializeApp(config),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- webapp/src/app/services/auth.service.ts
+++ webapp/src/app/services/auth.service.ts
@@ -1,9 +1,80 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+// firebase
+import * as firebaseui from 'firebaseui'
+import firebase from 'firebase/app'
+import { AngularFireAuth } from '@angular/fire/auth'
+import {
+    AngularFirestore,
+    AngularFirestoreDocument,
+    AngularFirestoreCollection,
+    DocumentReference,
+} from '@angular/fire/firestore'
+import { Observable, of } from 'rxjs'
+import { switchMap } from 'rxjs/operators'
+import { Router } from '@angular/router'
+
+// configuration for the ui
+const config = {
+    signInSuccessUrl: 'home',
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    tosUrl: 'terms-of-service',
+    privacyPolicyUrl: 'privacy-policy',
+}
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthService {
+    user$: Observable<any> = new Observable<any>()
+    user: any
+    ui: firebaseui.auth.AuthUI = new firebaseui.auth.AuthUI(firebase.auth()) // login firebase ui
+    constructor(
+        private afAuth: AngularFireAuth,
+        private afs: AngularFirestore,
+        private router: Router
+    ) {
+        this.getUser()
+    }
 
-  constructor() { }
+    async getUser() {
+        this.user$ = this.afAuth.authState.pipe(
+            switchMap((user: any) => {
+                // this.asyncOperation.next(false);
+                // Logged in
+                if (user) {
+                    this.user = user
+                    return of(this.user)
+                } else {
+                    // Logged out
+                    return of(null)
+                }
+            })
+        )
+    }
+    startUi() {
+        this.ui.start('#firebaseui-auth-container', config)
+    }
+    private updateUserData(user: any) {
+        // Sets user data to firestore on login
+        const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+            `users/${user.uid}`
+        )
+
+        const data = {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+        }
+
+        return userRef.set(data, { merge: true })
+    }
+
+    async signOut() {
+        await firebase.auth().signOut()
+        this.router.navigate(['/'])
+    }
 }
