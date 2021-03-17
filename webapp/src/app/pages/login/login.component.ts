47d32101137b854e98751a1567be5a4ab1ab3133
Davide Ghiotto <ghiotto.davidenko@gmail.com>
2021-03-17 19:28:04 +0100

    fixed login + btn glass style

--- webapp/src/app/pages/login/login.component.ts
+++ webapp/src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
+import { UtilsService } from '../../services/utils.service'
 import { User } from '../../models/user.model'
 import { AuthService } from '../../services/auth.service'
 
@@ -10,10 +11,16 @@ import { AuthService } from '../../services/auth.service'
 })
 export class LoginComponent implements OnInit {
     user: User = new User()
-    constructor(public auth: AuthService, public router: Router) {
+    constructor(
+        public auth: AuthService,
+        public router: Router,
+        private utils: UtilsService
+    ) {
+        this.utils.spinner.next(true)
         this.auth.user$.subscribe((user) => {
             this.user = user
-            if (this.user.email) this.router.navigateByUrl('home')
+            this.utils.spinner.next(false)
+            if (this.user && this.user.email) this.router.navigateByUrl('home')
             else this.auth.startUi()
         })
     }
